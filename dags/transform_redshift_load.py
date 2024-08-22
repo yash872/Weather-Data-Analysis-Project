0fb90a8e47c4890fb554fb51f45ae40e2859ff5f
yash872 <ridic143@gmail.com>
2024-08-22 10:48:41 +0530

    Added Images and dags, script

--- dags/transform_redshift_load.py
+++ dags/transform_redshift_load.py
@@ -18,11 +18,11 @@ dag = DAG('transform_redshift_dag', default_args=default_args, schedule_interval
 transform_task = GlueJobOperator(
     task_id='transform_task',
     job_name='glue_transform_task',
-    script_location='s3://aws-glue-assets-851725469799-us-east-1/scripts/weather_data_ingestion.py',
-    s3_bucket='s3://aws-glue-assets-851725469799-us-east-1',  # S3 bucket where logs and local etl script will be uploaded
+    script_location='s3://aws-glue-assets-010526265053-us-east-1/scripts/weather_data_ingestion.py',
+    s3_bucket='s3://aws-glue-assets-010526265053-us-east-1/',  # S3 bucket where logs and local etl script will be uploaded
     aws_conn_id='aws_default',  # You'll need to set up an AWS connection in Airflow
     region_name="us-east-1",
-    iam_role_name='glue-role',
-    create_job_kwargs ={"GlueVersion": "4.0", "NumberOfWorkers": 2, "WorkerType": "G.1X", "Connections":{"Connections":["Redshift-New-Connection"]},},
+    iam_role_name='AWSGlueServiceRole-fullAccess',
+    create_job_kwargs ={"GlueVersion": "4.0", "NumberOfWorkers": 2, "WorkerType": "G.1X", "Connections":{"Connections":["Redshift connection"]},},
     dag=dag,
 )
